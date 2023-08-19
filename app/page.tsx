ae4192cf1f11189c98fd382d487cabc8dfdba2d3
Guillermo Enrique Rodríguez Galban <93445026+guillerg01@users.noreply.github.com>
2023-08-19 16:09:45 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,231 @@
+
+'use client'
+import { useEffect, useState} from 'react'
+import axios from 'axios'
+import Image from 'next/image'
+import { ThemeProvider } from "@material-tailwind/react";
+import { Select, Option } from "@material-tailwind/react";
+import { useCookies } from 'react-cookie';
+
+export default function Home() {
+const [libros, setLibros] = useState<Object[]>([])
+const [librostotal, setLibrostotal] = useState<Object[]>([])
+const[cantidad,setCantidad] = useState<number>(0)
+const[lectura,setLectura] = useState<Object[]>([])
+const[generos,setGeneros] = useState<Object[]>(["Ninguno"])
+const[cantPaginas,setCantPaginas] = useState(0)
+const[cantidadLectura,setCantidadLectura] = useState<number>(0)
+
+
+
+const [cookies, setCookie] = useCookies(['lectura']);
+
+
+
+useEffect(()=>{
+
+const URL = 'http://localhost:3001/library'
+const respuesta = axios.get(URL).then((res)=>{
+  setLibros(res.data)
+  setCantidad(res.data.length)
+  console.log(res.data);
+  setLibrostotal(res.data)
+  setLectura(cookies.lectura)
+
+
+})
+},[])
+
+arreglarcat()
+
+
+  function arreglarcat() {
+
+    libros.map((l) => {
+
+      let yaesta = false;
+
+      generos.map((gen) => {
+
+        if (gen === l.book.genre) { yaesta = true }
+
+      })
+
+      !yaesta && setGeneros([...generos, l.book.genre])
+
+      yaesta = false
+
+    })
+
+
+  }
+
+useEffect(()=>{
+  setCantidadLectura(lectura.length)
+
+  lectura.length!==0&&setCookie('lectura', JSON.stringify(lectura), { path: '/' });
+  console.log(cookies.lectura);
+
+  
+  
+},[lectura])
+
+  useEffect(()=>{
+    libros.length===0&&setLibros(librostotal)
+  },[libros, librostotal])
+
+
+
+
+const filtrado = (e)=>{
+  setCantPaginas(e.target.value);
+  if(parseInt(e.target.value)<=42){ 
+    setLibros(librostotal);
+  }
+  else{
+
+  let newarreglo = librostotal.filter((l)=>{
+    return(l.book.pages<cantPaginas)
+  })
+  setLibros(newarreglo)
+
+  
+  }
+
+}
+
+
+
+  return (
+    <ThemeProvider >
+    <main className=' w-screen h-screen p-20'>
+      <div className='flex justify-center'>
+        <div className='lado isquierdo'>
+
+          <h1 className='text-white text-3xl'>{cantidad} libros disponibles</h1>
+          <h2 className='text-white text-xl'>{cantidadLectura  } en lista de lectura</h2>
+
+
+          <div className='flex flex-row row-2 gap-20 mt-10'>
+            <div>
+            <h2 className='text-white text-xl'> Filtrar por páginas  {cantPaginas!==0&&cantPaginas}</h2>
+            <input type="range" min={0} max="2000" value={cantPaginas} onChange={filtrado} className="accent-slate-500  my-4 bg-transparent " /> 
+            </div>
+            <div>
+            <h2 className='text-white text-xl'> Filtrar por género</h2>
+            <div className="flex w-40 h-auto my-2 flex-col bg-transparent gap-6">
+            {generos.length!==0&&<Select   onChange={(e) => {
+           
+             
+            
+             
+              setLibros(librostotal.filter((lib)=>{return (lib.book.genre === generos[e])}))
+              
+              }
+            
+            } 
+              
+              
+              color="blue" label="Seleciona el Genero">
+            
+       {generos.map((l,i)=>{
+        return(
+        <Option value={`${i}`} key={i}> {l}</Option>
+
+        
+       )})}
+       
+      </Select>}
+      </div>
+            </div>
+          </div>
+
+        <div className='grid grid-cols-4 transition-all '>
+          {libros.length!==0&&libros.map((l,i)=>{
+            return(
+              <Image key={i} 
+               onClick={()=>{lectura.filter((libro)=>{ return (libro===l) }).length===0 &&setLectura([...lectura,libros[i]]);}} 
+               alt='Imagenes'
+               className='mr-7 my-7' 
+               width={80} height={80}
+                src={l.book.cover}></Image>
+            )
+
+
+          })}
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <div className='ml-32 bg-gray-900 px-14 py-6 border-2 items-center justify-center relative h-screen border-gray-700 rounded-md'>
+      <h1 className='text-white text-3xl text-center'>Lista de Lectura</h1>
+        <div className='grid grid-cols-2'>
+       {lectura.length!==0&&lectura.map((l,i)=>{
+            
+            return(
+              <Image key={i} onClick={()=>{
+                
+                
+                
+                let arrnew = lectura.filter((l)=>{
+                  
+                 return( l!==lectura[i])
+                })
+                setLectura(arrnew)
+              
+              
+              
+              
+              
+              
+              
+              
+              }} alt='Imagenes'className='mr-7 my-7' width={80} height={80} src={l.book.cover}></Image>
+            )
+
+
+          })}
+          </div>
+      </div>
+      </div>
+    </main>
+    </ThemeProvider>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss'
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+const config= withMT( {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: { },
+  },
+  plugins: [],
+  
+  darkMode: 'class',
+})
+export default config
+
